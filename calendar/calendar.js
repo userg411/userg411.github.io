ef131682d36724b55b4c894479b84f398baeb3db
userg411 <userg411@gmail.com>
2017-08-19 14:59:05 +0500

    two employees support

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -3,8 +3,18 @@ var monthNames = ["January","February","March","April","May", "June", "July",
 var weekDays = ["Mo","Tu","We","Th","Fr","Sa", "Su"]; 
 var d = new Date();
 var year = d.getFullYear();
-var colNum = 3;
+var colNum = 2;
 var rowNum = monthNames.length/colNum;
+var intersectColor = "#F08080";
+var emps = [
+	{
+		name:'Employee 1',shift:'28',color:'#1ABC9C', selected:'true', startId:'0'
+	},
+	{
+		name:'Employee 2',shift:'28',color:'#87CEFA', selected:'false', startId:'0'
+	}
+];
+
 
 function addDay(date){
 	var result = new Date(date);
@@ -17,41 +27,40 @@ function getDay(date){
 	return date.getDay()-1;
 }
 
-function draw(){
+function drawCalendar(){
 	var id = 1;
 	var wrapper  = $('<div></div>');
+	var yearList = $('<ul></ul>').addClass('year');
+	
 	for (yr = year; yr<year+2; yr++){
-		var div = $('<div></div>');
+		var div = $('<li></li>');
 		div.attr('id','left');
 		if(yr!==year)
 			div.attr('id','right');
-		var caption = $('<caption></caption>').text(yr);
+		var caption = $('<div></div>').addClass('title').text(yr);
 		var date = new Date("01/01/"+yr);
-		var mainTable = $('<table></table>').addClass("mainTable");
+		var mainTable = $('<div></div>').addClass("mainTable");
 		mainTable.append(caption);
-		
+	
 		for(i=0; i<rowNum; i++){
-			var row = $('<tr></tr');
+			var row = $('<ul></ul');
 			for(j=0; j<colNum;j++){
-				var td = $('<td></td>');
-				var monthTable = $('<table></table').addClass("monthTable");
+				var td = $('<li></li>');
+				var monthTable = $('<div></div').addClass("monthTable");
+				var rowMonth = $('<div></div>').addClass('title').text(monthNames[i*colNum + j]);
+				var rowWeekDays = $('<ul></ul>').addClass("weekdays");
 				
-				
-				var rowMonth = $('<caption></caption>').text(monthNames[i*colNum + j]);
-				
-				var rowWeekDays = $('<tr></tr>').addClass("weekdays");
 				for(k=0; k<weekDays.length;k++){
-					var colWeekDay = $('<td></td>').text(weekDays[k]);
+					var colWeekDay = $('<li></li>').text(weekDays[k]);
 					rowWeekDays.append(colWeekDay);
 				}
 			    
 				monthTable.append(rowMonth).append(rowWeekDays);
 			    
-				
 				for(l=0; l<6;l++){
-					var rowWeek = $('<tr></tr>').addClass('days');
+					var rowWeek = $('<ul></ul>').addClass('days');
 					for(m=0; m<7; m++){
-						var day = $('<td></td>').text('\xa0');
+						var day = $('<li></li>').text('\xa0');
 						if(date.getMonth()==(i*colNum+j) && getDay(date) == m ){
 							day.text(date.getDate());
 							day.attr('id',  id++);
@@ -67,8 +76,8 @@ function draw(){
 			mainTable.append(row);
 		}
 		div.append(mainTable);
-		wrapper.append(div);
+		yearList.append(div);
 	}
-	
+	wrapper.append(yearList);
 	$( "body" ).append(wrapper);
 }
